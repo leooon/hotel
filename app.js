98bc0f9dc6e2ac814d0bfc51c4217023e3f82f98
Leon Santiago <leonpsantiago@gmail.com>
2025-01-18 15:05:06 -0300

    gfhgfhgf

--- app.js
+++ app.js
@@ -34,19 +34,32 @@ let hotel = {
 
 const dex = {
 	'clown': {
-		'unlocked': false,
-		'show': [],
-		'visit': [{'minAndares': 1}],
+		'show': {},
+		'visit': {
+			'floors': 1,
+		},
 	},
 	'dinossaur': {
-		'unlocked': false,
-		'show': [{'minAndares': 2}],
-		'visit': [{'minAndares': 3}],
+		'show': {
+			'guests': {
+				'clown': 1
+			},
+		},
+		'visit': {
+			'guests': {
+				'clown': 3
+			},
+		},
 	},
 	'ghost': {
-		'unlocked': false,
-		'show': [{'minAndares': 2}],
-		'visit': [{'minAndares': 3}]
+		'show': {
+			'guests': {
+				'dinosaur': 1
+			},
+		},
+		'visit': {
+			'floors': 2,
+		},
 	}
 }
 
@@ -58,6 +71,46 @@ let file = {
 }
 
 const actions = {
+	showChar: () => {
+		if (howMany({location: 'street'})>= 4) return;
+
+		const allowedTypes = [];
+		for (const type in dex) {
+			if (checkers.canShow(type)) allowedTypes.push(type);
+		}
+
+		const type = allowedTypes[helper.rand(0, allowedTypes.length - 1)];
+	
+		const id = Object.keys(file.chars).length ? Math.max(...Object.values(file.chars).map(char => char.id)) + 1 : 1;
+		const char = {
+			id: id,
+			type: type,
+			location: 'street',
+			el: actions.renderChar(type)
+		}
+		file.chars[id] = char;
+		save();
+	
+		actions.streetWalking(char);
+	},
+	renderChar(type) {
+		const el = document.createElement('div');
+
+		if (file.progress?.[type]?.visit) {
+			el.classList.add('char', type);
+		} else {
+			el.classList.add('char', 'locked');
+			el.innerHTML = '🔒';
+		}
+
+		document.getElementById('canvas').appendChild(el);
+
+		el.onclick = (e) => {
+			openDex(type);
+		};
+
+		return el;
+	},
 	streetWalking (char) {
 		const charName = char.type;
 		const el = char.el;
@@ -89,7 +142,7 @@ const actions = {
 				if (progress == 40 && !anim.control) {
 					anim.control = true;
 
-					if (canVisit(charName)) {
+					if (checkers.canVisit(charName)) {
 						el.classList.add('clown');
 					
 						anim.pause();
@@ -102,24 +155,118 @@ const actions = {
 	},
 	visiting (char) {
 		const order = Object.values(file.chars).filter(char => char.location === 'reception').length + 1;
-		
-		char.location = 'reception';
-		char.receptionOrder = order;
-		file.chars[char.id] = char;
-		save();
-		
+
+		const animation = anime({
+			targets: char.el,
+			easing: 'linear',
+			keyframes: [
+				{
+					duration: 1000,
+					translateY: getPosition('#escada').y,
+					translateX: getPosition('#escada').x
+				},
+				{
+					duration: 500,
+					translateY: getPosition('.gerente').y
+				},
+				{
+					duration: 500 * (5 - order),
+					translateX: getPosition('.gerente').x + (order * 35),
+				},
+			],
+			complete: () => {
+				openCheckin();
+				if (file.chars[char.id].receptionOrder != order) actions.adjustLine(char);
+			},
+		});
+
+		updateChar(char.id, {
+			location: 'reception',
+			receptionOrder: order,
+			animation: animation
+		});
+	},
+	adjustLine: (char) => {
 		anime({
 			targets: char.el,
-			keyframes: [
-				{translateY: getPosition('#escada').y, translateX: getPosition('#escada').x},
-				{translateY: getPosition('.gerente').y},
-				{translateX: getPosition('.gerente').x + (order * 35)},
-			],
-			duration: 4000,
+			duration: 200,
 			easing: 'linear',
+			translateX: getPosition('.gerente').x + (file.chars[char.id].receptionOrder * 35),
 			complete: openCheckin,
+		})
+	},
+	advanceLine: () => {
+		Object.values(file.chars)
+			.filter(char => char.location === 'reception')
+			.forEach(char => {
+				updateChar(char.id, {
+					receptionOrder: char.receptionOrder - 1
+				});
+
+				if (!char.animation || char.animation.completed) actions.adjustLine(char);
 		});
-	}
+	},
+	deny: () => {
+		const first = helper.firstInLine();
+
+		deleteChar(first.id);
+
+		first.el.style.zIndex = 99999;
+		
+		anime({
+			targets: first.el,
+			keyframes: [
+				{
+					scale: 9,
+					translateY: '-=100px',
+					translateX: `+=${Math.floor(Math.random() * 161) - 30}px`,
+					duration: 200,
+					easing: 'easeOutQuint'
+				}, {
+					translateY: '+=500px',
+					duration: 1200,
+					easing: 'easeInQuint'
+				}
+			],
+			complete: () => first.el.remove()
+		});
+		
+		document.getElementById('checkin').classList.remove('active');
+		if (Object.values(file.chars).some(char => char.location === 'reception')) actions.advanceLine();
+	},
+	checkIn: () => {
+		const first = helper.firstInLine();
+
+		const floor = file.building[1];
+		updateChar(first.id, {
+			location: 'hotel'
+		});
+
+		const porta = floor.el.querySelector('.porta');
+
+		anime({
+			targets: first.el,
+			easing: 'linear',
+			keyframes: [
+				{
+					duration: 1400,
+					translateX: getPosition('#recepcao .porta').x,
+				}, {
+					duration: 500,
+					opacity: 0,
+				}, {
+					duration: 500,
+					opacity: 1,
+					translateY: [getPosition(porta).y + 20, getPosition(porta).y + 20],
+				}, {
+					duration: 1000,
+					translateX: 120,
+				}
+			]
+		});
+
+		actions.advanceLine();
+	},
 }
 
 function openCheckin() {
@@ -139,102 +286,86 @@ function reset() {
 }
 
 window.addEventListener('load', () => {
-	const savedHotel = localStorage.getItem('hotel');
-	
-	if (savedHotel) {
-		hotel = JSON.parse(savedHotel);	
-	} else {
-		localStorage.setItem('hotel', JSON.stringify(hotel));
-	}
-
-	renderHotel();
-	
-	if (!localStorage.getItem('file')) {
-		document.querySelector('.fudido button').style.display = 'none';
-
-		mensagem('introducao', () => {
-			updateCoins(1000);
-			document.querySelector('.fudido button').style.display = 'block';
-		});
-		
-	} else {
-		file = JSON.parse(localStorage.getItem('file'));
-
-		document.getElementById('dinheiro').innerHTML = '🪙 ' + file.coins;
-		document.querySelector('.fudido button').style.display = 'block';
-	}
-
-	loadChars();
-
-	showCharsInterval();
-
-	document.querySelector('#reset').addEventListener('click', reset);
-	document.querySelector('#dex_bt').addEventListener('click', openDex);
-
-	if (Object.values(file.chars).some(char => char.location === 'reception')) {
-		openCheckin();
-	}
-	document.querySelector('#checkin_bt').addEventListener('click', ops.checkIn);
-	document.querySelector('#denied_bt').addEventListener('click', ops.deny);
+	load.loadFile();
+	load.building();
+	load.chars();
+	load.ux();
+	load.showCharInterval();
 });
 
-const ops = {
-	firstInLine: () => {
-		return Object.values(file.chars).filter(char => char.location === 'reception').reduce((highest, current) => {
-			return (highest.receptionOrder > current.receptionOrder) ? highest : current;
-		}, {receptionOrder: -1});
+const load = {
+	loadFile: () => {
+		if (!localStorage.getItem('file')) {
+			mensagem('introducao', () => {
+				updateCoins(1000);
+				document.querySelector('.fudido button').style.display = 'block';
+			});
+			
+		} else {
+			file = JSON.parse(localStorage.getItem('file'));
+			document.getElementById('dinheiro').innerHTML = '🪙 ' + file.coins;
+		}
 	},
-	checkIn: () => {
-		
-		
-	},
-	deny: () => {
-		const first = ops.firstInLine();
-
-		first.el.style.zIndex = 99999;
-		
-		anime({
-			targets: first.el,
-			keyframes: [
-				{
-					scale: 9,
-					translateY: '-=100px',
-					translateX: '+=60px',
-					duration: 300,
-					easing: 'easeOutQuint'
-				}, {
-					// scaleX: 1.5,
-					// scaleY: 0.5,
-					// duration: 300,
-					// easing: 'easeInQuad'
-				}, {
-					translateY: '+=500px',
-					duration: 1200,
-					easing: 'easeInQuad'
-				}
-			],
-			complete: function () {
-				first.el.remove();
-				delete file.chars[first.id];
-				save();
-			}
+	chars: () => {
+		Object.values(file.chars).filter(char => char.location === 'street').forEach(char => {
+			delete file.chars[char.id];
+		});
+	
+		Object.entries(file.chars).filter(([id, char]) => char.location === 'reception').forEach(([id, char]) => {
+			el = actions.renderChar(char.type);
+			file.chars[id].el = el;
+			el.style.transform = `translateY(${getPosition('.gerente').y}px) translateX(${getPosition('.gerente').x + (char.receptionOrder * 35)}px)`;
 		});
 	},
-	checkOut: () => {
+	building: () => {
+		const hotelEl = document.getElementById('hotel');
+
+		Object.entries(file.building).forEach(([key, floor]) => {
+			const template = document.querySelector('#andar').content;
+			const floorEl = template.firstElementChild.cloneNode(true);
+			floorEl.querySelector('p').innerHTML = hotel.andares[1].texto;
+			hotelEl.appendChild(floorEl);
+
+			file.building[key].el = floorEl;
+		});
+
+		const template = document.importNode(document.querySelector('#andar-fudido').content, true);
 		
+		const button = template.querySelector('button');
+		button.innerHTML = andar.preco;
+		button.addEventListener('click', comprarAndar);
+		
+		hotelEl.appendChild(template);
+		
+		const fudidoEl = document.querySelector('.fudido button');
+		const nextAndar = Object.values(hotel.andares).find(andar => !andar.comprado);
+		fudidoEl.innerHTML = nextAndar.preco;
+	},
+	ux: () => {
+		if (Object.values(file.chars).some(char => char.location === 'reception')) openCheckin();
+
+		document.querySelector('#reset').addEventListener('click', reset);
+		document.querySelector('#dex_bt').addEventListener('click', openDex);
+		document.querySelector('#checkin_bt').addEventListener('click', actions.checkIn);
+		document.querySelector('#denied_bt').addEventListener('click', actions.deny);
+	},
+	showCharInterval: () => {
+		actions.showChar();
+		setTimeout(load.showCharInterval, helper.rand(2000, 8000));
 	}
 }
 
-function loadChars() {
-	Object.values(file.chars).filter(char => char.location === 'street').forEach(char => {
-		delete file.chars[char.id];
-	});
+const helper = {
+	rand: (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+	firstInLine: () => {
+		const first = Object.values(file.chars)
+			.filter(char => char.receptionOrder > 0)
+			.reduce((highest, char) => {
+				return !highest || char.receptionOrder < highest.receptionOrder ? char : highest;
+			});
 
-	Object.entries(file.chars).filter(([id, char]) => char.location === 'reception').forEach(([id, char]) => {
-		el = renderChar(char.type);
-		file.chars[id].el = el;
-		el.style.transform = `translateY(${getPosition('.gerente').y}px) translateX(${getPosition('.gerente').x + (char.receptionOrder * 35)}px)`;
-	});
+		return first;
+	},
 }
 
 function mensagem(fluxo, callback) {
@@ -256,34 +387,6 @@ function mensagem(fluxo, callback) {
 	};
 }
 
-function renderHotel() {
-	const hotelEl = document.getElementById('hotel');
-
-	for (const [key, andar] of Object.entries(hotel.andares)) {
-		if (andar.comprado) {
-			const template = document.importNode(document.querySelector('#andar').content, true);
-			template.querySelector('p').innerHTML = andar.texto;
-			hotelEl.appendChild(template);
-		} else {
-			const template = document.importNode(document.querySelector('#andar-fudido').content, true);
-			
-			const button = template.querySelector('button');
-			button.innerHTML = andar.preco;
-			button.addEventListener('click', comprarAndar);
-			
-			hotelEl.appendChild(template);
-
-			break;
-		}
-	};
-	
-	const fudidoEl = document.querySelector('.fudido button');
-	const nextAndar = Object.values(hotel.andares).find(andar => !andar.comprado);
-	fudidoEl.innerHTML = nextAndar.preco;
-
-
-}
-
 function comprarAndar() {
 	const fudidoEl = document.querySelector('.fudido button');
 	const preco = parseInt(fudidoEl.innerHTML);
@@ -296,86 +399,23 @@ function comprarAndar() {
 		localStorage.setItem('hotel', JSON.stringify(hotel));
 
 		const hotelEl = document.getElementById('hotel');
-		const template = document.importNode(document.querySelector('#andar').content, true);
-		template.querySelector('p').innerHTML = hotel.andares[andar].texto;
-		hotelEl.insertBefore(template, hotelEl.querySelector('.fudido'));
+
+		const template = document.querySelector('#andar').content;
+		const floor = template.firstElementChild.cloneNode(true);
+		floor.querySelector('p').innerHTML = hotel.andares[andar].texto;
+		hotelEl.insertBefore(floor, hotelEl.querySelector('.fudido'));
+
+		const id = Object.keys(file.building).length ? Math.max(...Object.values(file.building).map(floor => floor.id)) + 1 : 1;
+
+		file.building[id] = {
+			id: id,
+			el: floor
+		}
+		save();
 	}
 }
 
 let sideToggle = false;
-
-function showChars() {
-	if (howMany('location', 'street')>= 4) return;
-	
-	const allowedTypes = [];
-
-	for (const type in dex) {
-		if (canShow(type)) allowedTypes.push(type);
-	}
-
-	const random = Math.floor(Math.random() * allowedTypes.length);
-	const type = allowedTypes[random];
-
-	const id = Object.keys(file.chars).length ? Math.max(...Object.values(file.chars).map(char => char.id)) + 1 : 1;
-	const char = {
-		id: id,
-		type: type,
-		location: 'street',
-		action: 'streetWalking',
-		el: renderChar(type)
-	}
-	file.chars[id] = char;
-	save();
-
-	actions.streetWalking(char);
-}
-
-function showCharsInterval() {
-	showChars();
-	
-	const rand = Math.floor(Math.random() * (8000 - 2000 + 1)) + 2000;
-	setTimeout(showCharsInterval, rand);
-}
-
-function canShow(type) {
-	let can = false;
-
-	if (!dex[type].show.length) can = true;
-
-	for (const showCondition of dex[type].show) {
-		const minAndares = showCondition.minAndares;
-		if (minAndares === undefined || Object.keys(hotel.andares).filter(key => hotel.andares[key].comprado).length >= minAndares) {
-			can = true;
-		}
-	}
-
-	if (can) {
-		file.progress[type] = file.progress[type] || {};
-		file.progress[type].show = true;
-		save();
-	}
-	
-	return can;
-}
-
-function renderChar(type) {
-	const el = document.createElement('div');
-
-	if (file.progress?.[type]?.visit) {
-		el.classList.add('char', type);
-	} else {
-		el.classList.add('char', 'locked');
-		el.innerHTML = '🔒';
-	}
-
-	document.getElementById('canvas').appendChild(el);
-
-	el.onclick = (e) => {
-		openDex(type);
-	};
-
-	return el;
-}
 
 function openDex (charName) {
 	const dexEl = document.getElementById('dex');
@@ -412,39 +452,65 @@ function closeDex () {
 }
 document.querySelector('#dex button').addEventListener('click', closeDex);
 
-function checkVisit(charEl) {
+const solve = {
+	'guests': (rules) => {
+		const ok = [];
+		for (const [type, min] of Object.entries(rules)) {
+			ok.push(howMany({type: type, location: 'hotel'}) >= min);
+		}
 	
+		return ok.every(value => value);
+	},
+	'floors': (min) => {
+		const floorCount = Object.entries(file.building).length;
+		return floorCount >= min;
+	}
 }
 
-function canVisit (type) {
-	if (Object.values(file.chars).filter(char => char.location === 'reception').length >= 4) return false;
+const checkers = {
+	canShow: (type) => {
+		const ok = [];
+		for (const [condition, rule] of Object.entries(dex[type].show)) {
+			ok.push(solve[condition](rule));
+		};
 
-	for (const visitCondition of dex[type].visit) {
-		const minAndares = visitCondition.minAndares;
-		if (minAndares === undefined || Object.keys(hotel.andares).filter(key => hotel.andares[key].comprado).length >= minAndares) {
-			
-			file.progress[type].visit = true;
-			save();
-
-			for (const id in file.chars) {
-				const char = file.chars[id];
-				if (char.type === type) {
-					char.el.classList.add(type);
-				}
+		if (ok.every(value => value)) {
+			if (!file.progress[type]?.show) {
+				file.progress[type] = {show: true};
+				save();
 			}
-			Object.values(file.chars).filter(char => char.type === type).forEach(char => {
-				char.el.classList.add(type);
-				char.el.innerHTML = '';
-			});
-
+			
 			return true;
 		}
+
+		return false;
+	},
+	canVisit: (type) => {
+		if (Object.values(file.chars).filter(char => char.location === 'reception').length >= 4) return false;
+
+		const ok = [];
+		for (const [condition, rule] of Object.entries(dex[type].visit)) ok.push(solve[condition](rule));
+
+		if (ok.every(value => value)) {
+			if (!file.progress[type].visit) {
+				file.progress[type].visit = true;
+				save();
+
+				Object.values(file.chars).filter(char => char.type === type).forEach(char => {
+					char.el.classList.add(type);
+					char.el.innerHTML = '';
+				});
+			}
+			
+			return true;
+		}
+
+		return false;
 	}
-	return false;
 }
 
 function getPosition(selector) {
-	const el = document.querySelector(selector);
+	const el = typeof selector === 'string' ? document.querySelector(selector) : selector;
 	const rect = el.getBoundingClientRect();
 	return {
 		x: rect.left + window.scrollX,
@@ -452,8 +518,13 @@ function getPosition(selector) {
 	};
 }
 
-function howMany(type, value) {
-	return Object.values(file.chars).filter(char => char[type] === value).length;
+function howMany(filters) {
+	return Object.values(file.chars).filter(char => {
+		for (const [prop, value] of Object.entries(filters)) {
+			if (char[prop] !== value) return false;
+		}
+		return true;
+	}).length;
 }
 
 function updateCoins(coins) {
@@ -461,4 +532,13 @@ function updateCoins(coins) {
 	save();
 
 	document.getElementById('dinheiro').innerHTML = '🪙 ' + file.coins;
+}
+
+function updateChar(id, update) {
+	Object.assign(file.chars[id], update);
+	save();
+}
+function deleteChar(id) {
+	delete file.chars[id];
+	save();
 }
